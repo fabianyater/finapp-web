2db0766b72c0ff55e5c2968c5b0a42c1e5212e67
Fabian Yate <fabianyate31@gmail.com>
2026-05-22 19:19:14 -0500

    feat: remove unused import from CategoryPicker component

--- src/pages/dashboard/components/CategoryPicker.tsx
+++ src/pages/dashboard/components/CategoryPicker.tsx
@@ -1,6 +1,5 @@
 import { X } from "lucide-react";
 import { type CategoryDto } from "@/api/categories";
-import { cn } from "@/lib/utils";
 import { resolveColor } from "../utils/colorUtils";
 
 export default function CategoryPicker({
